5dcc88e31fce7542473fb329e99d7f17ad5756db
shenyongqianmo <103726989+shenyongqianmo@users.noreply.github.com>
2022-05-01 14:16:29 +0800

    Update HelloWorldApplication.java
    第一次在github上对一个文件进行修改

--- src/main/java/com/wangsixuan/springboot/HelloWorldApplication.java
+++ src/main/java/com/wangsixuan/springboot/HelloWorldApplication.java
@@ -8,6 +8,9 @@ public class HelloWorldApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(HelloWorldApplication.class, args);
+        System.out.println("我成功拉了下来这行代码");
     }
+   
+    
 
 }
